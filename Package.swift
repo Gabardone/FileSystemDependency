3175d946168280f32e5e50b4c6a7dc71e518d4aa
Óscar Morales Vivó <oscarmv@mac.com>
2023-06-20 14:20:54 -0700

    Added package skeleton.

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FileSystemDependency",
+    platforms: [
+        // Minimum deployment version bound by `async/await` support.
+        .iOS(.v13),
+        .macCatalyst(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(
+            name: "FileSystemDependency",
+            targets: ["FileSystemDependency"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Gabardone/GlobalDependencies", from: "2.0.0")
+    ],
+    targets: [
+        .target(
+            name: "FileSystemDependency",
+            dependencies: ["GlobalDependencies"]
+        ),
+        .testTarget(
+            name: "FileSystemDependencyTests",
+            dependencies: ["FileSystemDependency"]
+        )
+    ]
+)
---
+++ Tests/FileSystemDependencyTests/FileSystemDependencyTests.swift
@@ -0,0 +1,11 @@
+@testable import FileSystemDependency
+import XCTest
+
+final class FileSystemDependencyTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FileSystemDependency().text, "Hello, World!")
+    }
+}
